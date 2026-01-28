2c9b00dced5f3ee3b2aba41af79120943d1c5474
ATUL KUMAR <98544302+Atul245@users.noreply.github.com>
2026-01-28 16:34:46 +0530

    Refactor notificationSender classes for correctness
    Fixed syntax errors in class definitions and updated output messages.

--- runTimePolymorphism.cpp
+++ runTimePolymorphism.cpp
@@ -1,24 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Interface (Abstract class)
 class notificationSender {
 public:
-    virtual void sendNotification(const string& message) = 0
+    virtual void sendNotification(const string& message) = 0;
+    virtual ~notificationSender() {}
 };
 
-class emailSender : public notificationSender () {
+// Email sender
+class emailSender : public notificationSender {
 public:
     void sendNotification(const string& message) override {
-        cout << "Message sent through Email..." << endl;
+        cout << "Email sent: " << message << endl;
     }
 };
 
-class  smsSender : public notificationSender () {
+// SMS sender
+class smsSender : public notificationSender {
 public:
     void sendNotification(const string& message) override {
-        cout << "Message sent through sms..." << endl;
+        cout << "SMS sent: " << message << endl;
     }
 };
 
 int main() {
-    
+    // Base class pointers (runtime polymorphism)
+    notificationSender* n1 = new emailSender();
+    notificationSender* n2 = new smsSender();
+
+    n1->sendNotification("Welcome to the system!");
+    n2->sendNotification("Your OTP is 123456");
+
+    delete n1;
+    delete n2;
 
     return 0;
 }
